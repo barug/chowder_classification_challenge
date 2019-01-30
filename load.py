bdc94d9c73ccda95df28f0570cc5df26b37a3380
barthelemy gouby <barthelemy.gouby@gmail.com>
2019-01-30 15:51:57 +0000

    new files for refactor

---
+++ load.py
@@ -0,0 +1,72 @@
+import argparse
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+class WSIFilesHandler:
+    
+    def __init__(self, data_dir, max_tiles_nbr):
+        assert data_dir.is_dir()
+        self.data_dir = data_dir
+        self.max_tiles_nbr = max_tiles_nbr
+
+    def load_features(self, filenames):
+        # Load numpy arrays
+        features = []
+        for f in filenames:
+            patient_features = np.load(f)
+
+            # Remove location features (but we could use them?)
+            patient_features = patient_features[:, 3:]
+            np.set_printoptions(threshold=np.inf)
+            np.set_printoptions(suppress=True)
+            pad_size = self.max_tiles_nbr - patient_features.shape[0]
+            left_pad = pad_size // 2
+            right_pad = pad_size // 2 + pad_size % 2
+            padded_features = np.pad(patient_features, ((left_pad, right_pad), (0,0)), mode='constant', constant_values=(0,))
+
+            padded_features = padded_features.transpose(1, 0)
+            
+            features.append(padded_features)
+
+        features = np.stack(features, axis=0)
+        return features
+
+
+    def load_dataset(self):
+        train_dir = self.data_dir / "train_input" / "resnet_features"
+        test_dir = self.data_dir / "test_input"  / "resnet_features"
+
+        train_output_filename = self.data_dir / "train_output.csv"
+        test_output_filename = self.data_dir / "test_output.csv"
+        train_output = pd.read_csv(train_output_filename)
+        test_output = pd.read_csv(test_output_filename)
+
+        # Get the filenames for train
+        filenames_train = [train_dir / "{}.npy".format(idx) for idx in train_output["ID"]]
+        for filename in filenames_train:
+            assert filename.is_file(), filename
+
+        # Get the numpy filenames for test
+        filenames_test = sorted(test_dir.glob("*.npy"))
+        for filename in filenames_test:
+            assert filename.is_file(), filename
+        self.ids_test = [f.stem for f in filenames_test]
+
+        features_train = self.load_features(filenames_train)
+        features_test = self.load_features(filenames_test)
+
+        # Get the labels
+        labels_train = train_output["Target"].values
+        labels_test = test_output["Target"].values
+
+        return features_train, features_test, labels_train, labels_test
+
+    def save_test_predictions(self, preds):
+        # Write the predictions in a csv file, to export them to the data challenge platform
+        test_output = pd.DataFrame({"ID": self.ids_test, "Target": preds})
+        test_output.set_index("ID", inplace=True)
+        test_output.to_csv(self.data_dir / "test_pytorch.csv")
+
+
---
+++ pytorch_metrics.py
@@ -0,0 +1,75 @@
+import torch 
+from torch import nn
+from sklearn import metrics
+
+
+def compute_forward_metrics(model, xb):
+    model.eval()
+    remove_handles = []
+
+    def apply_hook(m):
+        def forward_metrics(m, input, output):
+            act_metrics = {}
+            act_metrics["min"] = output.min().item()
+            act_metrics["max"] = output.max().item()
+            act_metrics["mean"] = output.mean().item()
+            act_metrics["std"] = output.std().item()
+            
+            if not hasattr(m, 'my_metrics'):
+                m.my_metrics = {}
+            m.my_metrics["forward"] = act_metrics
+        if isinstance(m, (nn.Conv1d, nn.Linear, nn.LeakyReLU)):
+            remove_handles.append(m.register_forward_hook(forward_metrics))
+
+    model.apply(apply_hook)
+    with torch.no_grad():
+        model(xb)
+
+    for rm_hook in remove_handles:
+        rm_hook.remove()
+
+def compute_backward_metrics(model, loss_func, xb, yb):
+    model.eval()
+    remove_handles = []
+
+    def apply_hook(m):
+        def backward_metrics(m, grad_input, output):
+            grad_metrics = {}
+            grad_metrics["min"] = output.min().item()
+            grad_metrics["max"] = output.max().item()
+            grad_metrics["mean"] = output.mean().item()
+            grad_metrics["std"] = output.std().item()
+            
+            if not hasattr(m, 'my_metrics'):
+                m.my_metrics = {}
+            m.my_metrics["grads"] = grad_metrics
+        if isinstance(m, (nn.Conv1d, nn.Linear, nn.LeakyReLU)):
+            remove_handles.append(m.register_forward_hook(backward_metrics))
+
+    model.apply(apply_hook)
+    y_pred = model(xb)
+    loss = loss_func(y_pred, yb)
+    loss.backward()
+
+    for rm_hook in remove_handles:
+        rm_hook.remove()
+
+def compute_weights_metrics(model):
+    def compute_lyr_weights(m):
+        if isinstance(m, (nn.Conv1d, nn.Linear, nn.Sigmoid)):
+            weights_metrics = {}
+            weights_metrics["min"] = m.weight.data.min().item()
+            weights_metrics["max"] = m.weight.data.max().item()
+            weights_metrics["mean"] = m.weight.data.max().item()
+            weights_metrics["std"] = m.weight.data.std().item()
+            if not hasattr(m, 'my_metrics'):
+                m.my_metrics = {}
+            m.my_metrics['weights'] = weights_metrics
+    model.apply(compute_lyr_weights)
+    
+
+def pred_metrics(labels_test, labels_pred):
+    labels_testset = labels_test.astype(int)
+    print("confusion matrix: \n" + str(metrics.confusion_matrix(labels_testset, labels_pred)))
+    print("accuracy : " + str(metrics.accuracy_score(labels_test, labels_pred)))
+
